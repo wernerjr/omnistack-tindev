1262a0f209f725c8ec28c39243275a7609d78bd8
Werner Gehrke Junior <werner.junior@outlook.com>
2019-08-08 00:08:45 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://tindev:tindev@cluster0-6unfh.mongodb.net/tindev?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
